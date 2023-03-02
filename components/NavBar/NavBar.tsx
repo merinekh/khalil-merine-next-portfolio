2de0582b7b4820fe069ea851cadfed343af159ee
Khalil Merine <merinekh@gmail.com>
2023-03-02 00:43:37 -0500

    NavLink Activated for Tablet and Desktop

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
 function NavBar({ darkMode, setdarkMode, isMenuOpen, handleMenuToggle }: any) {
+  const router = useRouter();
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto md:flex-nowrap">
@@ -51,45 +54,70 @@ function NavBar({ darkMode, setdarkMode, isMenuOpen, handleMenuToggle }: any) {
         >
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
+              <Link
                 href="/"
-                className="block py-2 pl-3 pr-4 text-white bg-teal-500 rounded md:bg-transparent md:text-teal-500 md:p-0 dark:text-white"
+                className={
+                  router.pathname == "/"
+                    ? "text-teal-500 " +
+                      "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                    : "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                }
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="/about"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={
+                  router.pathname == "/about"
+                    ? "text-teal-500 " +
+                      "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                    : "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                }
               >
                 About
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="/services"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={
+                  router.pathname == "/services"
+                    ? "text-teal-500 " +
+                      "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                    : "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                }
               >
                 Services
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="/portfolio"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={
+                  router.pathname == "/portfolio"
+                    ? "text-teal-500 " +
+                      "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                    : "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                }
               >
                 Portfolio
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="/contact"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={
+                  router.pathname == "/contact"
+                    ? "text-teal-500 " +
+                      "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                    : "block py-2 pl-3 pr-4 text-gray-700 bg-teal-500 rounded md:bg-transparent md:p-0 dark:text-white"
+                }
               >
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
