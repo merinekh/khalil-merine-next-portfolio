e745ba8cde2b840444cfb6a42805ceb135d89841
Khalil Merine <merinekh@gmail.com>
2023-03-02 23:13:02 -0500

    Responsiveness Problem Solved

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -11,16 +11,21 @@ import web6 from "../../public/web6.png";
 export default function Project() {
   return (
     <>
-      <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+      <div className="flex flex-col flex-nowrap gap-10 py-10 md:flex-row md:flex-wrap">
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider">Title</h1>
-            <p className="mx-auto">Description</p>
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+            <h1 className="tracking-wider lg:p-1 font-semibold">
+              Air Canada Industry Project
+            </h1>
+            <p className="mx-2 text-xs lg:p-2 mb-2">
+              I collaborated with a multidisiplinary team on enhancing customer
+              experience for a diverse user base and increasing engagement.
+            </p>
             <button
               type="button"
-              className=" items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
+              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
             >
-              Resume
+              Take A Look at MyCode
             </button>{" "}
           </a>
           <a href="#" className="relative">
@@ -35,9 +40,14 @@ export default function Project() {
           </a>
         </div>
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider">Title</h1>
-            <p className="mx-auto">Description</p>
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+            <h1 className="tracking-wider"> Air Canada Industry Project</h1>
+            <p className="mx-2 text-xs ">
+              {" "}
+              I collaborated with a team of UX designers and data scientists to
+              build a pop-up and preference form, enhancing customer experience
+              for a diverse user base and increasing engagement.
+            </p>
             <button
               type="button"
               className=" items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
@@ -58,7 +68,7 @@ export default function Project() {
         </div>
 
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
             <h1 className="tracking-wider">Title</h1>
             <p className="mx-auto">Description</p>
             <button
@@ -81,7 +91,7 @@ export default function Project() {
         </div>
 
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
             <h1 className="tracking-wider">Title</h1>
             <p className="mx-auto">Description</p>
             <button
@@ -104,7 +114,7 @@ export default function Project() {
         </div>
 
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
             <h1 className="tracking-wider">Title</h1>
             <p className="mx-auto">Description</p>
             <button
@@ -127,7 +137,7 @@ export default function Project() {
         </div>
 
         <div className="relative basis-1/3 flex-1">
-          <a className="absolute rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+          <a className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
             <h1 className="tracking-wider">Title</h1>
             <p className="mx-auto">Description</p>
             <button
