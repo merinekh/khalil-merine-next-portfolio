a6d4e3fea022e71ac273f2076e6228dda129db7e
Khalil Merine <merinekh@gmail.com>
2024-01-13 17:20:58 -0500

    Fix bugs + Update

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -8,183 +8,136 @@ import web2 from "../../public/web2.png";
 import web5 from "../../public/web5.png";
 import web6 from "../../public/web6.png";
 
+const projects = [
+  {
+    title: "Surface Nouvelle Ère",
+    description:
+      "Developed a cutting-edge website for 'Surface Nouvelle Ère,' a Montreal-based countertop business, using Next.js to enhance their digital presence and showcase their craftsmanship.",
+    githubLink: "https://www.github.com/merinekh/surface_nouvelle_ere",
+    imageSrc: web6,
+    technologies: ["Next.js", "React", "Tailwind CSS"],
+    features: [
+      "Visually stunning galleries showcasing countertop designs.",
+      "Intuitive navigation for exploring countertop styles and materials.",
+      "Optimal performance and responsive design across devices.",
+      "SEO enhancements for increased online visibility.",
+    ],
+  },
+  {
+    title: "BrainFlix",
+    description:
+      "Implemented a full-stack solution for 'BrainFlix,' a video-sharing platform, using Next.js, React, and Node.js to provide server-side functionality and a seamless user experience.",
+    githubLink: "https://www.github.com/merinekh/khalil-merine-brainflix",
+    imageSrc: web5,
+    technologies: ["Next.js", "React", "Node.js", "Express"],
+    features: [
+      "Single-page clone of YouTube with dynamic video page updates.",
+      "User authentication and video upload capabilities.",
+      "Proficient full-stack development demonstrating dynamic and functional web applications.",
+    ],
+  },
+  {
+    title: "Bandsite",
+    description:
+      "Crafted the front-end of a site resembling Ticketmaster for a seamless ticket booking experience. Utilized Next.js, React, and Tailwind CSS for optimal user interaction.",
+    githubLink: "https://www.github.com/merinekh/khalil-merine-bandsite",
+    imageSrc: web4,
+    technologies: ["Next.js", "React", "Bootstrap"],
+    features: [
+      "User-friendly interface for viewing upcoming shows.",
+      "Effortless ticket booking experience.",
+      "Expertise in front-end development for event-based websites.",
+    ],
+  },
+  {
+    title: "Air Canada Industry Project",
+    description:
+      "Collaborated on enhancing customer experience and engagement for Air Canada. Utilized a multidisciplinary approach and the latest technologies to deliver effective solutions.",
+    githubLink: "https://www.github.com/merinekh/",
+    imageSrc: web1,
+    technologies: ["Collaborative Multidisciplinary Approach"],
+    features: [
+      "Enhanced customer experience for a diverse user base.",
+      "Increased engagement through effective solutions.",
+    ],
+  },
+  {
+    title: "DailyFit",
+    description:
+      "Developed a comprehensive single-page web app for personalized meal and workout planning. Utilized React, Sass, Bootstrap, Node.js, and Express for a robust full-stack solution.",
+    githubLink: "https://www.github.com/merinekh/my-fitness-app",
+    imageSrc: web2,
+    technologies: ["React", "Sass", "Bootstrap", "Node.js", "Express"],
+    features: [
+      "Personalized meal and workout planning.",
+      "Proficiency in full-stack development.",
+      "Creating a comprehensive platform for user engagement.",
+    ],
+  },
+  {
+    title: "InStock",
+    description:
+      "Collaborated on building a full-stack web app for inventory and warehouse management. Utilized the latest technologies and API integration for a seamless user experience.",
+    githubLink: "https://www.github.com/merinekh/siar-instock-client",
+    imageSrc: web3,
+    technologies: ["Full-stack Development", "API Integration"],
+    features: [
+      "Full-stack web app for inventory and warehouse management.",
+      "Seamless user experience through API integration.",
+      "Demonstrated ability to deliver effective solutions.",
+    ],
+  },
+];
+
 export default function Project() {
   return (
-    <>
-      <div className="flex flex-col flex-nowrap gap-10 py-10 md:flex-row md:flex-wrap">
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">
-              Air Canada Industry Project
-            </h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              I collaborated with a multidisiplinary team on enhancing customer
-              experience for a diverse user base and increasing engagement.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
+    <div className="flex flex-wrap gap-10 justify-center m-8">
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="relative w-72 rounded-lg overflow-hidden shadow-md cursor-pointer transform hover:scale-105 transition-transform"
+        >
+          {index < 3 && (
+            <div className="text-black text-sm bg-white px-2">
+              *Scrollable Image
+            </div>
+          )}
+          <div
+            className={`w-full ${
+              index < 3 ? "h-36 overflow-y-auto" : "h-50"
+            } relative`}
+          >
+            <Image
+              className="object-cover w-full h-full"
+              alt=""
+              layout="responsive"
+              src={project.imageSrc}
+            />
           </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web1}
-              />
+          <div className="flex flex-col h-auto justify-between p-4 bg-white rounded">
+            <div>
+              <h1 className="text-lg font-semibold mb-2">{project.title}</h1>
+              <p className="text-sm overflow-hidden mb-4">
+                {project.description}
+              </p>
+            </div>
+            <div>
+              <h2 className="text-md font-semibold mb-2">Technologies Used:</h2>
+              <ul className="list-disc list-inside mb-2">
+                {project.technologies.map((tech, index) => (
+                  <li key={index}>{tech}</li>
+                ))}
+              </ul>
+              <h2 className="text-md font-semibold mb-2">Key Features:</h2>
+              <ul className="list-disc list-inside">
+                {project.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">DailyFit </h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              I developed a single-page web app with React, Sass, and Bootstrap
-              for personalized meal and workout planning. the back-end built
-              with Node.js and Express to store user information and profiles.
-              This project demonstrates my proficiency in full-stack development
-              and ability to create a comprehensive platform for users seeking a
-              personalized experience.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/my-fitness-app"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
-          </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web2}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-fit sm:h-full md:h-fit xl:h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">InStock </h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              Collaborated with a team to build a full-stack web app for
-              inventory and warehouse management, utilizing an API for seamless
-              user experience. Demonstrates my ability to deliver effective
-              solutions using the latest technologies.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/siar-instock-client"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
-          </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-fit sm:h-full md:h-fit xl:h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">Travel Site</h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              Built the front-end for a travel agency, creating a visually
-              appealing and user-friendly interface. Demonstrates my expertise
-              in front-end development for the travel industry.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
-          </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web6}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">BrainFlix </h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              I built a single-page clone of YouTube with server-side
-              functionality, allowing for storage of video and user information.
-              With dynamic changes to the video page upon selection, users can
-              upload videos and login to their accounts with ease. This project
-              demonstrates my proficiency in full-stack development and ability
-              to create dynamic and functional web applications.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/khalil-merine-brainflix"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
-          </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="relative basis-1/3 flex-1">
-          <div className="absolute h-full rounded-lg inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
-            <h1 className="tracking-wider lg:p-1 font-semibold">Bandsite </h1>
-            <p className="mx-2 text-xs lg:p-2 mb-2">
-              I built the front-end of a site similar to Ticketmaster, allowing
-              users to easily view upcoming shows in their area and book
-              tickets. This project showcases my expertise in front-end
-              development and ability to create user-friendly interfaces for
-              ticket booking websites.
-            </p>
-            <a
-              href="https://www.github.com/merinekh/khalil-merine-bandsite"
-              target="_blank"
-              className="m-2 items-center bg-gradient-to-r from-cyan-500 to-teal-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-teal-500 dark:focus:ring-blue-800"
-            >
-              Take A Look at MyCode
-            </a>{" "}
-          </div>
-          <div className="relative">
-            <div className="h-100 flex flex-wrap content-center">
-              <Image
-                className="rounded-lg object-cover z-0 w-full h-100"
-                alt=""
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
